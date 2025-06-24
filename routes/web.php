58780e4d25d308115a22326087f4795ee1c5e472
Solaiman Hossain <solaiman0168@gmail.com>
2025-06-24 20:26:57 +0600

    url error path page add

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('app');
 });
+
+
+Route::get('/{pathMatch}', function () {
+    return view('app');
+})->where('pathMatch', '.*');
